746347951f27473a1605f72ec7d272ad8c93b939
WolfTech Innovations <SpoinkOSGithub@gmail.com>
2025-02-15 17:57:55 +0000

    commit

--- script.bash
+++ script.bash
@@ -2,4 +2,4 @@ yarn add nvm && nvm install 18 && nvm use 18 && nvm alias default 18 && node -v
 echo Installed Node.JS 18.x.x
 yarn add @nuxt/kit@latest
 yarn global add @nuxt/cli
-nuxt build
+yarn build
